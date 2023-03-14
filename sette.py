723af3344911033e47be515f920c3da358a04c5f
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2023-03-14 11:10:48 +0000

    fino a es 7

---
+++ sette.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+import datetime
+
+@app.route('/')
+
+def index():
+  return render_template("index6.html", Ora=datetime.datetime.now())
+
+@app.route('/mappa')
+def mappa():
+  return render_template("index5.html", Ora=datetime.datetime.now())
+
+@app.route('/mappa600')
+def mappa600():
+  return render_template("index7.html", dimensione = 600)
+
+@app.route('/mappa800')
+def mappa800():
+  return render_template("index7.html", dimensione = 800)
+
+@app.route('/mappa1000')
+def mappa1000():
+  return render_template("index7.html", dimensione = 1000)
+
+@app.route('/mappa/<width>')
+def mappawidth(width):
+  return render_template("index7.html", dimensione = width)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
